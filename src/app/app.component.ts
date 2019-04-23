ea662d4c2062df1c3f348f5f89491c20959cece2
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-23 15:28:13 +0300

    Add new class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
+// COMPONENT ANNOTATION add functionality to our code
 @Component({
-  selector: 'app-root',
+  selector: 'gibs',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+// COMPONENT DEFINITION CLASS normal js for logic
 export class AppComponent {
-  title = 'Goals';
+  //constructor(){}
+  // goal = 'Watch Finding Nemo'
+  goals: string[];
+
+  constructor(){
+    this.goals = ["Watch Finding Nemo","Go to the mall","Weed the flowers"]
+  }
 }
