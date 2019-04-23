23cf6ebecadb2f28a37d8d2cdb3799e8ec0068e6
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-23 16:33:19 +0300

    add a new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,10 @@ import { Goal } from './goal';
 
 // COMPONENT DEFINITION CLASS normal js for logic
 export class AppComponent {
-  //constructor(){}
+  constructor(){}
   // goal = 'Watch Finding Nemo'
   // goals: string[];
-
   // constructor(){
   //   this.goals = ["Watch Finding Nemo","Go to the mall","Weed the flowers"]
   // }
-
-  goals = [
-    new Goal(1, 'Watch Avengers: End Game', 0),
-    new Goal(2, "Sell mail stamps", 1),
-    new Goal(3, "Polish brass placements", 0),
-    new Goal(4, "Practise shooting hoops", 1),
-    new Goal(5, "Entertain the cats", 0)
-    ]
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
-  selector: 'app-goal',
+  selector: 'gabs',
   templateUrl: './goal.component.html',
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+
+  goals = [
+    new Goal(1, 'Watch Avengers: End Game', 0),
+    new Goal(2, "Sell mail stamps", 1),
+    new Goal(3, "Polish brass placements", 0),
+    new Goal(4, "Practise shooting hoops", 1),
+    new Goal(5, "Entertain the cats", 0)
+    ]
 
   constructor() { }
 
