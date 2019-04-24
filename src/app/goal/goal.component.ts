2442d61b90d800fa612bd14e3b93635a4411de88
Phil Kipkemboi <kipkemboiphil@gmail.com>
2019-04-24 12:32:00 +0300

    Add completion date to the tasks

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,11 +9,11 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
 
   goals = [
-    new Goal(1, 'Watch Avengers: End Game', "Catch it at a nearby theater"),
-    new Goal(2, "Sell mail stamps", "Aim at reaching all the denizens of Sucsville"),
-    new Goal(3, "Polish brass placements", "The goal is intricate stuff"),
-    new Goal(4, "Practise shooting hoops", "Get better at it than all the neighbourhood hoodlums"),
-    new Goal(5, "Entertain the cats", "Any soul can do it, right?")
+    new Goal(1, 'Watch Avengers: End Game', "Catch it at a nearby theater", new Date(2019,8,28)),
+    new Goal(2, "Sell mail stamps", "Aim at reaching all the denizens of Sucsville", new Date(2019,10,18)),
+    new Goal(3, "Polish brass placements", "The goal is intricate stuff", new Date(2019,5,9)),
+    new Goal(4, "Practise shooting hoops", "Get better at it than all the neighbourhood hoodlums", new Date(2019,1,14)),
+    new Goal(5, "Entertain the cats", "Any soul can do it, right?", new Date(2019,11,4))
     ]
 
     completeGoal(isComplete, index){
